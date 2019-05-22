0b44f470de59593b4e74cf71ba294b3a9866c52f
Michael <m3k0c6@gmail.com>
2019-05-22 19:11:15 -0400

    moved data files, setup flask, ratings for resumes now sorted

--- alpha.py
+++ alpha.py
@@ -2,15 +2,17 @@ import textract
 from textblob import TextBlob
 import pdb
 import os 
+from collections import OrderedDict
+from operator import itemgetter
 
-JOB_SPECS = ['2019_JobSpec_AutomationLead.docx', '2019_JobSpec_SRE.docx', '2019_JobSpec_InfraMgr.docx', '2019_JobSpec_Oracle.docx']
+JOB_SPECS = os.listdir("Job_Specs")
 with open('skills.txt') as f:
 	skills = f.read().split('\n')
 
 skills = [s.lower() for s in skills]
 
 #results = []
-job_requirements = [textract.process(j).decode('utf8') for j in JOB_SPECS]
+job_requirements = [textract.process("Job_Specs/" + j).decode('utf8') for j in JOB_SPECS]
 keywords = dict()
 for i in range(len(job_requirements)):
 	#match_dict = {JOB_SPECS[i]:matches}
@@ -28,32 +30,22 @@ resumes.remove("Resumes.zip")
 print(resumes)	
 
 resume_text = [textract.process("Resumes/" + r).decode('utf8').lower() for r in resumes]
-
-
 results = dict()
 
 for job_name,matches in keywords.items():
 	scores = []
 	for i in range(len(resume_text)):
 		score = sum([resume_text[i].count(keyword) for keyword in matches ])
-		scores.append({resumes[i]:score})
-	results[job_name] = scores
-#print reuslts in descending order 
+		scores.append({"name":resumes[i],"score":score})
+	sorted_scores = sorted(scores,key=lambda i: i["score"],reverse=True)
+	results[job_name] = sorted_scores
+#print results in descending order 
+
+#sorted_results = sorted(,key = itemgetter("score"))
 pdb.set_trace()
 
 
 
 
 
-
-
-# y =j.split('.')
-# #y = spacy.explain("obj")
-# #pdb.set_trace()
-#blob = TextBlob(x)
-# #print(blob.tags)
-
-# for d in y:
-# 	d = TextBlob(d)
-# 	print(d.tags)
 
